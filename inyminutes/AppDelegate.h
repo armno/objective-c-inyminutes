fc2aab3666073d9d2d10e783ad63300c107207dc
Armno <monkeyarmno@gmail.com>
2014-06-20 17:17:33 +0700

    Initial Commit

---
+++ inyminutes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  inyminutes
+//
+//  Created by ArmNo on 6/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
